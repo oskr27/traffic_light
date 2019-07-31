6b33fe21970e1734ef4a1efc14191833f7740462
5260a81d6e14daaae5ccc4b508b05552767cafbd6 <mariosc_1991@hotmail.com>
2019-07-31 15:23:32 +0900

    Adding the following changes: 1. Addition of a new parameter in the random_dataset_creator module. The user is now able    to create a training-dataset-sample by stating the --generate_sample parameter to True 2. Creation of Jupyter Notebook "cnn_trainer-no-warnings", which store the results of the    experiment of training the CNN using warning as stop, and warningLeft as stopLeft 3. Modification of .gitignore to checkout those files in the duplicated dataset

--- traffic_light/utils/random_dataset_creator.py
+++ traffic_light/utils/random_dataset_creator.py
@@ -14,11 +14,17 @@ def is_selected(probability):
 
 
 def copy_images(lisa_dataset_dir, bosch_dataset_dir, dtld_dataset_dir, output_dir, generate_sample):
+    # Cleaning output directory
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    if generate_sample and os.path.exists(str(output_dir) + '-sample'):
+        shutil.rmtree(str(output_dir) + '-sample')
+
     images = []
 
-    lisa_modifier = 0.10
+    lisa_modifier = 0.1
     bosch_modifier = 1.0
-    dtld_modifier = 0.5
+    dtld_modifier = 1.0
 
     if lisa_dataset_dir != '':
         lisa_images = [file for file in lisa_dataset_dir.glob('**/*.*')]
@@ -30,6 +36,8 @@ def copy_images(lisa_dataset_dir, bosch_dataset_dir, dtld_dataset_dir, output_di
         dtld_images = [file for file in dtld_dataset_dir.glob('**/*.*') if 'Right' not in str(file.parent)]
         images += random.sample(dtld_images, ir(len(dtld_images) * dtld_modifier))
 
+    # Then, we need to create the dataset using the six classes go, goLeft, stop, stopLeft, warning, and warningLeft
+    # For this, we need to have a base of around 1000 images per class (2000 would be ideal)
     for image in images:
         if str(lisa_dataset_dir) in str(image):
             output = str.replace(str(image), str(lisa_dataset_dir), str(output_dir))
@@ -39,12 +47,12 @@ def copy_images(lisa_dataset_dir, bosch_dataset_dir, dtld_dataset_dir, output_di
             output = str.replace(str(image), str(dtld_dataset_dir), str(output_dir))
 
         # Merging warning to stop
-        if not os.path.exists(Path(output.replace('warning', 'stop')).parent):
-            os.makedirs(Path(output.replace('warning', 'stop')).parent)
+        if not os.path.exists(Path(output).parent):
+            os.makedirs(Path(output).parent)
 
-        shutil.copy(image, output.replace('warning', 'stop'))
+        shutil.copy(image, output)
 
-        if is_selected(0.05) and generate_sample:
+        if is_selected(0.01) and generate_sample:
             if str(lisa_dataset_dir) in str(image):
                 sample_output = str.replace(str(image), str(lisa_dataset_dir), str(output_dir) + '-sample')
             elif str(bosch_dataset_dir) in str(image):
@@ -52,10 +60,10 @@ def copy_images(lisa_dataset_dir, bosch_dataset_dir, dtld_dataset_dir, output_di
             elif str(dtld_dataset_dir) in str(image):
                 sample_output = str.replace(str(image), str(dtld_dataset_dir), str(output_dir) + '-sample')
 
-            if not os.path.exists(Path(sample_output.replace('warning', 'stop')).parent):
-                os.makedirs(Path(sample_output.replace('warning', 'stop')).parent)
+            if not os.path.exists(Path(sample_output).parent):
+                os.makedirs(Path(sample_output).parent)
 
-            shutil.copy(image, sample_output.replace('warning', 'stop'))
+            shutil.copy(image, sample_output)
 
 
 def parse_args():
